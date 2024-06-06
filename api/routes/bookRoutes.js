af0a865526472dd52a3b5dbce9548bcc5163fb66
Surya Narayana Gaddam <144539989+Suryanarayana-gaddam@users.noreply.github.com>
2024-06-06 16:21:48 +0530

    updated del route

--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const { getBookById, getAllBooks, getBooksByCreaterId, getBooksByCategory, uploadABook, updateBook, deleteBook, getSearchedBooks} = require("../controllers/bookControllers")
 const verifyToken = require("../middleware/verifyToken");
 
-router.get('/books/:id', verifyToken, getBookById);
+router.get('/book/:id', verifyToken, getBookById);
 router.get('/all-books',  getAllBooks);
 router.get('/user/:createrId/get/books', verifyToken, getBooksByCreaterId);
 router.get('/all-books/bycategory', verifyToken, getBooksByCategory);
 router.get('/all-books/searchedbooks', verifyToken, getSearchedBooks);
 router.post('/upload-book', verifyToken, uploadABook);
 router.patch('/book/update/:id', verifyToken,updateBook);
-router.delete('/book/:id', verifyToken,deleteBook);
+router.delete('/book/delete/:id', verifyToken,deleteBook);
 
 
 module.exports= router;
